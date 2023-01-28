94f677ca73a3daf4c0ae0bd6d074a75ff1c2634e
vinay kumar <122643407+Vinaykumar860964@users.noreply.github.com>
2023-01-29 00:12:14 +0530

    java identifier
    lets achieve our goal with each other

---
+++ JAVA DSA SYSTEM design course PHYSICS WALLAH/Identifier.java
@@ -0,0 +1,11 @@
+public class Identifier {
+    
+    public static void main(String[] args) {
+        
+        int age=32;
+        String firstName="vinay";
+             // camel case rule :- firstName
+        System.out.println(age);
+        System.out.println(firstName);
+    }
+}
